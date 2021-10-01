0c4c4100f3fdab95c5bb68dd6b9461c1ad27030f
Lindsay Phung <90883776+lindsayp30@users.noreply.github.com>
2021-10-01 06:30:08 -0400

    Add files via upload

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -7,13 +7,13 @@ HW08 -- Refactor Freshie Zero / updated greeting message last HW to a function t
 DISCOVERIES
 - I created a different file named <Greet02.java> which had the same code as <Greet.java>. I encountered the error
 "class Greet is public, should be declared in a file named Greet.java"
-which I now know means that I should have kept working on the <Greet.java> since the class name and the filename has to match.
+which I now know means that I should have kept working on the <Greet.java> since the class name and the filename has to match
 - I previously just typed in <public static void greet(String)> without typing in an identifier
-- I need to remember to add semicolons.
-- I remembered that concantenation occurs when operator <+>
+- I need to remember to add semicolons
+- I remembered that concantenation occurs when using operator <+> with Strings
 
 UNRESOLVED QUESTIONS
-- Is it just semicolons at the end of each line if it ends with a parentheses (not including lines with brackets at the end)?
+- Is it just semicolons at the end of each line if the line ends with a parentheses (not including lines with brackets at the end)?
 */
 
 public class Greet {
