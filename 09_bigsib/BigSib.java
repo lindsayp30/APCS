759fcea66e396c2a88ea5128c940aef549511cc5
Lindsay Phung <90883776+lindsayp30@users.noreply.github.com>
2021-10-04 19:11:12 -0400

    Add files via upload

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,17 @@
+/*
+Mangos -- Andrew Piatetsky / Lindsay Phung (Tommy)/ Joseph Othman
+APCS
+HW09 -- What Are BigSibs Good For? / updated HW08
+2021-10-04
+
+DISCOVERIES
+- still need to remember to add semicolons at the end of lines
+UNRESOLVED QUESTIONS
+- String cannot be converted to String[]
+*/
+
+public class BigSib {
+    public static void greet( String x ) {
+        System.out.println( x + ", are you okay?");
+    }
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/*
+Mangos -- Andrew Piatetsky / Lindsay Phung (Tommy)/ Joseph Othman
+APCS
+HW09 -- What Are BigSibs Good For? / updated HW08
+2021-10-04
+
+DISCOVERIES
+- still need to remember to add semicolons at the end of lines
+UNRESOLVED QUESTIONS
+- String cannot be converted to String[]
+*/
+
+public class Greet {
+	public static void main( String[] args ) {
+		BigSib.greet("Rando");
+		BigSib.greet("Person01");
+		BigSib.greet("Person02");
+	}
+}
