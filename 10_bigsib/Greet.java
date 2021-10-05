ae5b6e480723a1e3a61a8138858f0e3fff36c7ea
Lindsay Phung <90883776+lindsayp30@users.noreply.github.com>
2021-10-05 06:04:05 -0400

    Add files via upload

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,20 @@
+/*
+Mangos -- Andrew Piatetsky (Burnt Peanut)/ Lindsay Phung (Tommy)/ Joseph Othman (Po)
+APCS
+HW10 -- Refactor Big Sib One / updated HW09 such that now Greet.java prints the output instead of BigSib.java
+2021-10-05
+
+DISCOVERIES
+- String cannot be converted to String[] --> data type change from void to String
+- since Greet.java already has the System.out.println, we don't have to include it in this file? --> change to return
+UNRESOLVED QUESTIONS
+- 'void' type not allowed here
+*/
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
