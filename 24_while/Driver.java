b5b65e49edead4b46ca294fee1c5871f5d7755f2
Lindsay Phung <90883776+lindsayp30@users.noreply.github.com>
2021-10-26 13:11:49 -0400

    Add files via upload

--- 24_while/Driver.java
+++ 24_while/Driver.java
@@ -1,48 +1,74 @@
 /*
 Home-coming -- Ariel Fuchs / Lindsay Phung
 APCS
-HW 24 -- Get It While You Can
-2021-10-26
-time spent: 1
+HW 23 -- What Does Equality Look Like? / First Driver + Navigator Work Via Coins
+2021-10-24
+time spent: 1.1
 
 DISCO
-- this() is in red
+(Josiah Moltz's contribution)
+- precond is what is needed to run a method
+- postcond is what happens after running a method
+
+- accessors ... take no parameters and have a return statement... give value of the accessed variable
+- accessors vs. mutators ... the former accesses data while the latter alters the data
+- = vs. == ... the former acts as an operator that ASSIGNS a value to the variable while the latter compares the two variables to see if their values are the same or not
+
+- Math.random returns a floating number between 0.0 and 1.0
 QCC
-- reviewed BigSib and Greet work
-- supposedly compiles but takes forever to run
+- is the only efficient way to stimulate a coin flip the usage of Math.random?
 */
+
+
+/***
+    driver for class Coin
+    ~~~ SUGGESTED WORKFLOW: ~~~
+    1. Compile this file and run. Note anything notable.
+    2. Move the "TOP" line down, so that it is below the first statement.
+    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
+    (your editor: ???)
+    3. Compile and run again.
+    4. Resolve errors one at a time until it works.
+    5. Repeat 2-4 until TOP meets BOTTOM.
+***/
 
 public class Driver {
 
   public static void main( String[] args ) {
+
+    //build Objects from blueprint specified by class Coin
+
+    //test default constructor
     Coin mine = new Coin();
+
+    //test 1st overloaded constructor
     Coin yours = new Coin( "quarter" );
 
-    int x = 5;
-    int y = 10;
-    int numHEADS = 0;
-    int numMATCHES = 0;
+   //test 2nd overloaded constructor
+    Coin wayne = new Coin( "dollar", "heads" );
 
-    while (numHEADS < x) {
-      mine.flip();
-    }
+   //test toString() methods of each Coin
+    System.out.println("mine: " + mine);
+    System.out.println("yours: " + yours);
+    System.out.println("wayne: " + wayne);
 
-    while (numMATCHES < y) {
-      mine.flip();
-      yours.flip();
-      if (mine.equals(yours)) {
-        numMATCHES += 1;
-      }
-    }
+    //test flip() method
+    System.out.println("\nAfter flipping...");
+    yours.flip();
+    wayne.flip();
+    System.out.println("yours: " + yours);
+    System.out.println("wayne: " + wayne);
 
-    while ((numMATCHES < 65536) && (numMATCHES % 2005 != 0))  {
-      mine.flip();
-      yours.flip();
-      if (mine.equals(yours)) {
-        numMATCHES += 1;
-      }
+    //test equals() method
+    if ( yours.equals(wayne) ) {
+      System.out.println( "Matchee matchee!" );
     }
-    System.out.println("number of heads: " + numHEADS);
-    System.out.println("number of matches : " + numMATCHES);
-  }
-}
+    else {
+      System.out.println( "No match. Firestarter you can not be." );
+    }
+    /*===================TOP==========================
+    ====================BOTTOM======================*/
+
+  } //end main()
+
+} //end class
