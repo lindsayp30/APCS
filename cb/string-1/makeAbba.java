e7a60c637a7b16f799bc63b9d2cbb74f441b1306
Lindsay Phung <90883776+lindsayp30@users.noreply.github.com>
2021-11-01 05:15:12 -0400

    Add files via upload

--- cb/string-1/makeAbba.java
+++ cb/string-1/makeAbba.java
@@ -1,4 +1,15 @@
-public String makeAbba(String a, String b) {
-  return a + b + b + a;
-}
+public class makeAbba {
+    public static void main(String[] args) {
+        System.out.println(makeAbba("Hi", "Bye")); // "HiByeByeHi"
+        System.out.println(makeAbba("Yo", "Alice")); // "YoAliceAliceYo"
+        System.out.println(makeAbba("What", "Up")); // "WhatUpUpWhat"
+    }
 
+    /**
+     * Given two strings, a and b, return the result of putting them together in
+     * the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+     */
+    public static String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+}
