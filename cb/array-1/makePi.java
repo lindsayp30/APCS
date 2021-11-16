f7deafd7d8c90848d3320cd156beb26bebb3be8d
Lindsay <lphung30@stuy.edu>
2021-11-16 13:19:10 -0500

    classwork

---
+++ cb/array-1/makePi.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class makePi {
+	
+	public static int[] makePi() {
+		int[] pi = {3, 1, 4};
+		return pi;
+	}
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(makePi()));
+	}
+}
