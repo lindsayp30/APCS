f9caac7c79448a2988024f1817a4d3f913da062c
Lindsay <lphung30@stuy.edu>
2022-03-27 14:43:06 -0400

    rollin

---
+++ 82_/V6Driver.java
@@ -0,0 +1,37 @@
+// Rowing Lemurs and Leopards (Ruby Friedman, Lindsay Phung, Lawrence Joa)
+// APCS pd7
+// HW82 -- Roll Your Own Iterator
+// 2022-03-28m
+// time spent: 0.5 hr
+
+/***
+ * class V6Driver
+ * ...for testing implementation of Iterable interface
+ *    (ability to run a FOREACH loop over your LList)
+ *
+ * Assumes local version of List.java, overriding Java standard library interface
+ **/
+
+public class V6Driver
+{
+  public static void main( String[] args )
+  {
+    List<String> wu = new LList<String>();
+
+    System.out.println(wu);
+    wu.add("RZA");
+    wu.add("GZA");
+    wu.add("ODB");
+    wu.add("Inspectah Deck");
+    wu.add("Raekwon the Chef");
+    wu.add("U-God");
+    wu.add("Ghostface");
+    wu.add("Method Man");
+    System.out.println(wu);
+
+    for( String s : wu ) {
+      System.out.println( s + " raps for the Wu");
+    }
+  }//end main()
+
+}//end class
