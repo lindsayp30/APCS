c9fa8260088a52086bf85756d4c92cd5b6fb0166
Lindsay <lphung30@stuy.edu>
2022-03-29 13:19:12 -0400

    cw

---
+++ 84_stack/LatKtS.java
@@ -0,0 +1,52 @@
+// Rowing Lemurs and Leopards (Ruby Friedman, Lindsay Phung, Lawrence Joa)
+// APCS pd7
+// HW84 -- Stack: What Is It Good For?
+// 2022-03-30w
+// time spent: 0.5 hr
+
+/***
+ * class LatKtS
+ * SKELETON
+ * driver class for Latkes
+ * Uses a stack to reverse a text string, check for sets of matching parens.
+ **/
+
+public class LatKtS
+{
+
+  /***
+   * precondition:  input string has length > 0
+   * postcondition: returns reversed string s
+   *                flip("desserts") -> "stressed"
+   **/
+  public static String flip( String s )
+  {
+  }
+
+
+  /***
+   * precondition:  s contains only the characters {,},(,),[,]
+   * postcondition: allMatched( "({}[()])" )    -> true
+   *                allMatched( "([)]" )        -> false
+   *                allMatched( "" )            -> true
+   **/
+  public static boolean allMatched( String s )
+  {
+  }
+
+
+  //main method to test
+  public static void main( String[] args )
+  {
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+    System.out.println(flip("stressed"));
+    System.out.println(allMatched( "({}[()])" )); //true
+    System.out.println(allMatched( "([)]" ) ); //false
+    System.out.println(allMatched( "(){([])}" ) ); //true
+    System.out.println(allMatched( "](){([])}" ) ); //false
+    System.out.println(allMatched( "(){([])}(" ) ); //false
+    System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+  }
+
+}//end class
